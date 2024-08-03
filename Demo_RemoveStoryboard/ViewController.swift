a7b7b4ea6dd5a16da56f22384bef7700d748208d
Min Hu <lebonthe@gmail.com>
2024-08-03 22:19:03 +0800

    Initial Commit

---
+++ Demo_RemoveStoryboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Demo_RemoveStoryboard
+//
+//  Created by Min Hu on 2024/8/3.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
